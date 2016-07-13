219e274df588c6cf703fec71ad092bbc64e7b71d
rjgamer <jonathan.nessier@outlook.com>
2016-07-13 14:22:02 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,27 +1,21 @@
-# jquery.insertloader
+# jQuery InsertLoader
 jQuery-based helper for loading JS and CSS on the fly
 
-'''
-// ******************************************
-// BISHERIGE FUNKTION
-// *************************************
-
-// Eine Datei laden
+## Examples
+```javascript
+// Loading one file
 $.insert('https://example.com/demo.js');
 
-// ******************************************
-// NEUE FUNKTIONEN
-// ******************************************
+// Loading one file with a base-url
+$.insert('path/to/foo.js', 'http://foo.com/path/to/bar/');
 
-// Mehrere Dateien von der selben Basis-URL laden
+// Loading multiple files
+$.insert(['https://foo.com/bar.css', 'http://lorem.de/ipsum.js']);
+
+// Loading multiple files with the same base-url
 var moduleFiles = [
     'path/to/foo.css',
     'path/to/bar.js'
 ];
 $.insert(moduleFiles, 'https://foo.com/bar/');
-
-// Mehrere Dateien mit unterschiedlichen URLs laden
-$.insert(['https://foo.com/bar.css', 'http://lorem.de/ipsum.js']);
-
-// Eine Datei von einer Basis-URL laden
-$.insert('path/to/foo.js', 'http://foo.com/path/to/bar/');
+```
---
+++ src/jquery-insert.js
@@ -0,0 +1,70 @@
+/*!
+ * InsertLoader 1.0
+ *
+ * Copyright (c) 2016 Jonathan Nessier (https://www.neoflow.ch)
+ * Released under The MIT license (MIT)
+ */
+var InsertLoader = (function () {
+
+    this.load = function (urls, path) {
+        if (!path || typeof path === 'undefined' || (typeof path === 'string' && path.length)) {
+            if ($.isArray(urls)) {
+                return loadResources(urls, path);
+            } else if (typeof urls === 'string') {
+                return loadResource(urls, path);
+            }
+            throw new Error('Multiple URLs have to be in an array or only one URL has to be a string');
+        }
+        throw new Error('Path has to be a string or empty');
+    };
+
+    var loadResource = function (url, path) {
+        if (typeof url === 'string' && url.length) {
+
+            var extension = url.substring(url.lastIndexOf('.') + 1, url.length),
+                    finalUrl = (path || '') + url + '?_=' + new Date().getTime(),
+                    $head = $('head');
+
+            switch (extension) {
+                case 'js':
+                    $head.append($('<script>', {
+                        src: finalUrl
+                    }));
+                    break;
+                case 'css':
+                    $head.append($('<link>', {
+                        href: finalUrl,
+                        rel: 'stylesheet',
+                        type: 'test/css'
+                    }));
+                    break;
+                default:
+                    console.warn('URL is not linking to a CSS or JS file: ' + url);
+                    return false;
+            }
+            return true;
+        }
+        throw new Error('URL has to be a string');
+    };
+
+    var loadResources = function (urls, path) {
+        if ($.isArray(urls)) {
+            var result = true;
+            $.each(urls, $.proxy(function (i, url) {
+                if (!loadResource(url, path)) {
+                    result = false;
+                }
+            }, this));
+            return result;
+        }
+        throw new Error('Multiple URLs have to be in an array');
+    };
+
+    return {
+        load: load
+    };
+})();
+
+$.insert = function (urls, path) {
+    return InsertLoader.load(urls, path);
+};
